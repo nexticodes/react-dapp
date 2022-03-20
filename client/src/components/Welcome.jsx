66f698299a9c48c74fdc90439062d7f3d288ed87
Ernest Bruno <erndobruno@gmail.com>
2022-03-20 09:07:54 -0700

    Create form that accepts addressTo, message, a keyword and the amount of eth to send.

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -6,9 +6,25 @@ import { Loader } from './';
 
 const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
+const Input = ({placeholder, name, type, value, handleChange}) => (
+    <input 
+        placeholder={placeholder}
+        type={type}
+        step="0.0001"
+        value={value}
+        onChange={(e) => handleChange(e, name)}
+        className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
+    />
+);
+
+
 const Welcome = () => {
 
     const connectWallet = () => {
+
+    }
+
+    const handleSubmit = () => {
 
     }
 
@@ -64,7 +80,18 @@ const Welcome = () => {
                                 </div>
                             </div>
                         </div>
-
+                        <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
+                            <Input placeholder="Address To" name="addressTo" type="text" handleChange={() => {}} />
+                            <Input placeholder="Amount in Ether" name="amount" type="number" handleChange={() => {}} />
+                            <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={() => {}} />
+                            <Input placeholder="Enter Message" name="message" type="text" handleChange={() => {}} />
+                            <div className="h-[1px] w-full bg-gray-400 my-2"/>
+                            {true ? (
+                                <Loader />
+                            ) : (
+                                <button className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-full cursor-pointer" type="button" onClick={handleSubmit}>Send Now</button>
+                            )}
+                        </div>
                     </div>
                 </div>
             </div>
