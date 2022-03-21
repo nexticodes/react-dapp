d2571102ee2dd53651f7a3d97929226bc43ca095
Ernest Bruno <erndobruno@gmail.com>
2022-03-21 14:36:50 -0700

    Conditionally render button.

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -24,7 +24,7 @@ const Input = ({placeholder, name, type, value, handleChange}) => (
 
 const Welcome = () => {
 
-    const { connectWallet } = useContext(TransactionContext);
+    const { connectWallet, currentAccount } = useContext(TransactionContext);
 
     const handleSubmit = () => {
 
@@ -40,9 +40,11 @@ const Welcome = () => {
                     <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
                         Explore the crypto world. Buy and sell cryptocurrencies easily on Krypto.
                     </p>
-                    <button type="button" onClick={connectWallet} className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]">
-                        <p className="text-white text-base font-bold">Connect Wallet</p>
-                    </button>
+                    {!currentAccount && (
+                        <button type="button" onClick={connectWallet} className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]">
+                            <p className="text-white text-base font-bold">Connect Wallet</p>
+                        </button>
+                    )}
                     <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
                         <div className={`rounded-tl-2xl ${commonStyles}`}>
                             Reliability
