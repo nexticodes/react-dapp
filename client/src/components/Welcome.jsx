8a1e608ce7edfe75c6d7d57ac22c24c028d6eebb
Ernest Bruno <erndobruno@gmail.com>
2022-03-21 14:53:03 -0700

    Retrieve data from context, along with sendTransaction method that currently displays what the contract we created looks like when 'Send Now' button is pressed.

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -24,10 +24,14 @@ const Input = ({placeholder, name, type, value, handleChange}) => (
 
 const Welcome = () => {
 
-    const { connectWallet, currentAccount } = useContext(TransactionContext);
+    const { connectWallet, currentAccount, formData, setFormData, handleChange, sendTransaction } = useContext(TransactionContext);
 
-    const handleSubmit = () => {
+    const handleSubmit = (e) => {
+        const { addressTo, amount, keyword, message } = formData;
+        e.preventDefault();
 
+        if (!addressTo || !amount || !keyword || !message) return;
+        sendTransaction();
     }
 
     return (
@@ -86,12 +90,12 @@ const Welcome = () => {
                             </div>
                         </div>
                         <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
-                            <Input placeholder="Address To" name="addressTo" type="text" handleChange={() => {}} />
-                            <Input placeholder="Amount in Ether" name="amount" type="number" handleChange={() => {}} />
-                            <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={() => {}} />
-                            <Input placeholder="Enter Message" name="message" type="text" handleChange={() => {}} />
+                            <Input placeholder="Address To" name="addressTo" type="text" handleChange={handleChange} />
+                            <Input placeholder="Amount in Ether" name="amount" type="number" handleChange={handleChange} />
+                            <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={handleChange} />
+                            <Input placeholder="Enter Message" name="message" type="text" handleChange={handleChange} />
                             <div className="h-[1px] w-full bg-gray-400 my-2"/>
-                            {true ? (
+                            {false ? (
                                 <Loader />
                             ) : (
                                 <button className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-full cursor-pointer" type="button" onClick={handleSubmit}>Send Now</button>
