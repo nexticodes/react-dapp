2548a5c2e9b2cb525d70081db0b55c320c5ca60e
Ernest Bruno <erndobruno@gmail.com>
2022-03-21 15:09:39 -0700

    Successfully send ethereum from one address to another.

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -12,11 +12,7 @@ const getEthereumContract = () => {
     const signer = provider.getSigner();
     const transactionContract = new ethers.Contract(contractAddress, contractABI, signer);
 
-    console.log({
-        provider,
-        signer,
-        transactionContract
-    });
+    return transactionContract;
 };
 
 export const TransactionProvider = ({ children }) => {
@@ -28,6 +24,8 @@ export const TransactionProvider = ({ children }) => {
         keyword: '',
         message: ''
     });
+    const [isLoading, setIsLoading] = useState(false);
+    const [transactionCount, setTransactionCount] = useState(localStorage.getItem('transactionCount'));
 
     const handleChange = (e, name) => {
         setFormData((prevState) => ({...prevState, [name]: e.target.value}));
@@ -70,7 +68,38 @@ export const TransactionProvider = ({ children }) => {
         try {
             if (!ethereum) return alert("Please install Metamask!");
             const { addressTo, amount, keyword, message } = formData;
-            getEthereumContract();
+            const transactionContract = getEthereumContract();
+            // PARSE THE DECIMAL AMOUNT FROM FORM TO GWEI.
+            const parsedAmount = ethers.utils.parseEther(amount);
+
+            console.log(formData);
+            // Send Ethereum
+            await ethereum.request({
+                method: 'eth_sendTransaction',
+                params: [{
+                    from: currentAccount,
+                    to: addressTo,
+                    gas: '0x5208', // hex,  equals to 21000 Gwei = 0.000021 Ether
+                    value: parsedAmount._hex,
+                }]
+            });
+
+            // Store our transaction in the blockchain.
+            // Returns transactionHash 
+            const transactionHash = await transactionContract.addToBlockchain(addressTo, parsedAmount, message, keyword);
+
+            setIsLoading(true);
+            console.log(`Loading - ${transactionHash.hash}`);
+
+            // wait for transactionHash to finish.
+            await transactionHash.wait();
+
+            setIsLoading(false);
+            console.log(`Success - ${transactionHash.hash}`);
+
+            const transactionCount = await transactionContract.getTransactionCount();
+            setTransactionCount(transactionCount.toNumber());
+
         } catch (e) {
             console.log(e);
             throw new Error("No ethereum object!");
